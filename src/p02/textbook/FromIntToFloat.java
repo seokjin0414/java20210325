d6a0b96277690a9fddc2c0e2da8dbbe25da967a5
seokjin0414 <sars21@hanmail.net>
2021-03-29 09:48:22 +0900

    9:47

--- src/p02/textbook/FromIntToFloat.java
+++ src/p02/textbook/FromIntToFloat.java
@@ -7,7 +7,8 @@ public class FromIntToFloat {
 		
 		float num3 = num2;
 		num2 = (int) num3;
-		
+		System.out.println(num3);
+		System.out.println(num2);
 		int result = num1 - num2;
 		System.out.println(result);
 	}
