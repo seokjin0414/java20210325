48b902e50ee29c3d11851e744880efda3741aa4f
seokjin0414 <sars21@hanmail.net>
2021-03-30 10:41:42 +0900

    10:41

--- src/p03/textbook/ConditionalOperationExample101.java
+++ src/p03/textbook/ConditionalOperationExample101.java
@@ -1,6 +1,6 @@
 package p03.textbook;
 
-public class ConditionalOperationExample {
+public class ConditionalOperationExample101 {
 	public static void main(String[] args) {
 		int score = 85;
 		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'c');
