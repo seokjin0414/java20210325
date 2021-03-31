3dc3bf0777f03adf0e8ddc3cd1ddcac1f9fb87f1
seokjin0414 <sars21@hanmail.net>
2021-03-31 15:24:07 +0900

    3:24

--- src/p04/textbook/ForMultiplicationTableExample2.java
+++ src/p04/textbook/ForMultiplicationTableExample2.java
@@ -27,6 +27,13 @@ public class ForMultiplicationTableExample2 {
 		 **
 		 *
 		 */
+		System.out.println("---------------------");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < (5 - i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		/*
 		 *****
 		  ****
@@ -34,6 +41,16 @@ public class ForMultiplicationTableExample2 {
 		    **
 		     *
 		 */
+		System.out.println("---------------------");
+		for (int i = 0; i < 5; i++) {
+			for (int k = 0; k < i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < (5 - i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		/*
 		     *
 		    **
@@ -41,6 +58,16 @@ public class ForMultiplicationTableExample2 {
 		  ****
 		 *****
 		 */
+		System.out.println("---------------------");
+		for (int i = 0; i < 5; i++) {
+			for (int k = 0; k < (4 - i); k++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
 }
