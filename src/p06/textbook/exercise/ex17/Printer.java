426b97ad6326178cae39edfa3a0d37795ec39727
seokjin0414 <sars21@hanmail.net>
2021-04-08 12:38:31 +0900

    12:38

---
+++ src/p06/textbook/exercise/ex17/Printer.java
@@ -0,0 +1,19 @@
+package p06.textbook.exercise.ex17;
+
+public class Printer {
+	public static void println(int x) {
+		System.out.println(x);
+	}
+
+	public static void println(boolean x) {
+		System.out.println(x);
+	}
+
+	public static void println(double x) {
+		System.out.println(x);
+	}
+
+	public static void println(String x) {
+		System.out.println(x);
+	}
+}
